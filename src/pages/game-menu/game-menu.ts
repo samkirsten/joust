8eb75e7cf4807f64fe1d7abecf988ec1cb4ab2b9
Sam Kirsten <sam.kirsten@gmail.com>
2018-01-04 22:11:47 +0000

    Added game menu buttons

--- src/pages/game-menu/game-menu.ts
+++ src/pages/game-menu/game-menu.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {HostGamePage} from "../host-game/host-game";
 
 /**
  * Generated class for the GameMenuPage page.
--- www/build/main.js
+++ www/build/main.js
@@ -741,11 +741,12 @@ var GameMenuPage = (function () {
     };
     GameMenuPage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
-            selector: 'page-game-menu',template:/*ion-inline-start:"/Users/samkirsten/Projects/joust/src/pages/game-menu/game-menu.html"*/'<!--\n  Generated template for the GameMenuPage page.\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n  Ionic pages and navigation.\n-->\n<ion-header>\n\n  <ion-navbar>\n    <ion-title>gameMenu</ion-title>\n  </ion-navbar>\n\n</ion-header>\n\n\n<ion-content padding>\n\n</ion-content>\n'/*ion-inline-end:"/Users/samkirsten/Projects/joust/src/pages/game-menu/game-menu.html"*/,
+            selector: 'page-game-menu',template:/*ion-inline-start:"/Users/samkirsten/Projects/joust/src/pages/game-menu/game-menu.html"*/'<!--\n  Generated template for the GameMenuPage page.\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n  Ionic pages and navigation.\n-->\n<ion-header>\n\n  <ion-navbar>\n    <ion-title>New Game</ion-title>\n  </ion-navbar>\n\n</ion-header>\n\n\n<ion-content padding>\n    <button ion-button round outline>Host Game</button>\n    <button ion-button round outline>Join Game</button>\n</ion-content>\n'/*ion-inline-end:"/Users/samkirsten/Projects/joust/src/pages/game-menu/game-menu.html"*/,
         }),
-        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* NavController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavParams */]])
+        __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* NavController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* NavController */]) === "function" && _a || Object, typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavParams */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavParams */]) === "function" && _b || Object])
     ], GameMenuPage);
     return GameMenuPage;
+    var _a, _b;
 }());
 
 //# sourceMappingURL=game-menu.js.map
